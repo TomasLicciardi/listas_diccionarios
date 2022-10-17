3ba7c8f2ae0be73ba92a372c8cc18552ed911d27
TomasLicciardi <t.licciardi@alumno.um.edu.ar>
2022-10-17 13:45:36 -0300

    Lista_diccionarios con CircleCi y CodeClimate

--- lista_diccionarios.py
+++ lista_diccionarios.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 def repetidos(lista):
     dic_num = {}
     for i in lista:
@@ -10,25 +7,3 @@ def repetidos(lista):
             dic_num[i] = 1
 
     return dic_num    
-
-
-
-class Testupper(unittest.TestCase):
-    def test_1(self):
-        list = repetidos([1,2,3])
-        self.assertEqual(list, {1:1, 2:1, 3:1})
-
-    def test_2(self):
-        list = repetidos([1,1,2,3])
-        self.assertEqual(list, {1:2, 2:1, 3:1})
-
-    def test_3(self):
-        list = repetidos([1,1,1,1,2,3])
-        self.assertEqual(list, {1:4, 2:1, 3:1})
-
-    def test_4(self):
-        list = repetidos([1,1,1,1,2,2,3])
-        self.assertEqual(list, {1:4, 2:2, 3:1})
-
-if __name__ == '__main__':
-    unittest.main()
